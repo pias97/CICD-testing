78303e0fe39ddb86e85459cc32963db96d656317
pias97 <ranabir.devnath@gmail.com>
2024-11-01 01:10:19 +0600

    terraform ec2 module added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,16 @@
-provider "aws"{
-    alias = "App1"
-    region = "us-east-1"
+module "App1" {
+  source = "./modules/ec2"
+  ami_value = "ami-0866a3c8686eaeeba"                 #use your preffered ami
+  instance_type_value = "t2.micro"                    #use your preffered instance type
+  subnet_id_value = "subnet-0394c8cbfe3b035e0"        #use your preffered subnet
+  region = "us-east-1"
 }
 
-provider "aws"{
-    alias = "App2"
-    region = "us-east-2"
-}
 
-resource "aws_instance" "example1" {
-  ami = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  provider = aws.App1
-  subnet_id = "subnet-0394c8cbfe3b035e0"
-  key_name = "usual-login"
-}
-
-resource "aws_instance" "example2" {
-  ami = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  provider = aws.App2
-  subnet_id = "subnet-0394c8cbfe3b035e0"
-  key_name = "usual-login"
+module "App2" {
+  source = "./modules/ec2"
+  ami_value = "ami-0ea3c35c5c3284d82"                 #use your preffered ami
+  instance_type_value = "t2.micro"                    #use your preffered instance type
+  subnet_id_value = "subnet-0aff09ffc2869df62"        #use your preffered subnet
+  region = "us-east-2"
 }
