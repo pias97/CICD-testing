79eb810544ed16406e3ae44da3aa51e45fbb67f0
pias97 <ranabir.devnath@gmail.com>
2024-10-31 21:01:32 +0600

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,25 @@
 provider "aws"{
+    alias = "App1"
     region = "us-east-1"
 }
 
-resource "aws_instance" "ec2" {
+provider "aws"{
+    alias = "App2"
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example1" {
   ami = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
+  provider = aws.App1
+  subnet_id = "subnet-0394c8cbfe3b035e0"
+  key_name = "usual-login"
+}
+
+resource "aws_instance" "example2" {
+  ami = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  provider = aws.App2
   subnet_id = "subnet-0394c8cbfe3b035e0"
   key_name = "usual-login"
 }
